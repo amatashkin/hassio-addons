e41243e35463863ce345b4fda08b82f30fc672a5
Alexey Matashkin <amatashkin@gmail.com>
2020-09-03 00:57:32 +1000

    Adds host and hostID options

--- snapclient/run.sh
+++ snapclient/run.sh
@@ -3,12 +3,21 @@
 declare -a options
 
 # Set custom server name if provided
-if bashio::config.has_value 'snapserver'; then
-    options+=(-h $(bashio::config 'snapserver'))
-    bashio::log.info "Connecting to $(bashio::config 'snapserver')"
+if bashio::config.has_value 'host'; then
+    options+=(--host $(bashio::config 'host'))
+    bashio::log.info "Connecting to $(bashio::config 'host')"
 else
-    options+=(-h $(bashio::info.hostname))
+    options+=(--host $(bashio::info.hostname))
     bashio::log.info "Connecting to HomeAssistant"
+fi
+
+# Set custom client name if provided
+if bashio::config.has_value 'hostID'; then
+    options+=(--hostID $(bashio::config 'hostID'))
+    bashio::log.info "Connecting as $(bashio::config 'hostID')"
+else
+    options+=(--hostID HomeAssistant)
+    bashio::log.info "Connecting as HomeAssistant"
 fi
 
 echo "Starting Snapclient..."
